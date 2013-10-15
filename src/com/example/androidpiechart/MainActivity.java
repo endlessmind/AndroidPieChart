1b380acd732a6348df66a6638d35af74dfc236aa
endlessmind <the_mr_hb@hotmail.com>
2013-10-15 13:12:12 +0200

    Added example

--- src/com/example/androidpiechart/MainActivity.java
+++ src/com/example/androidpiechart/MainActivity.java
@@ -6,10 +6,46 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	PieChart pieChart;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		pieChart = (PieChart) findViewById(R.id.pieChart1);
+		
+		pieChart.setMax(100);
+		PiePiece p = new PiePiece();
+		p.value = 5;
+		p.Text = "Text 1";
+		pieChart.addPiece(p);
+		
+		p = new PiePiece();
+		p.value = 7;
+		p.Text = "Text 2";
+		pieChart.addPiece(p);
+		
+		p = new PiePiece();
+		p.value = 13;
+		p.Text = "Text 3";
+		pieChart.addPiece(p);
+		
+		p = new PiePiece();
+		p.value = 15;
+		p.Text = "Text 4";
+		pieChart.addPiece(p);
+		
+		p = new PiePiece();
+		p.value = 20;
+		p.Text = "Text 5";
+		pieChart.addPiece(p);
+		
+		p = new PiePiece();
+		p.value = 40;
+		p.Text = "Text 6";
+		pieChart.addPiece(p);
+		
+		
 	}
 
 	@Override
